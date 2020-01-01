a3f5fad55c0102de63c015ec8a8867f0eb3ba5fb
tx-TEM <highhigheggman@yahoo.co.jp>
2020-01-01 10:34:24 +0900

    タブViewを仮生成

--- NReader/RootView.swift
+++ NReader/RootView.swift
@@ -2,7 +2,24 @@ import SwiftUI
 
 struct RootView: View {
     var body: some View {
-        Text("Hello, World!")
+        TabView {
+            Text("Home Tab")
+                .tabItem {
+                    Image(systemName: "1.square.fill")
+                    Text("Home")
+                }
+            Text("Ranking Tab")
+                .tabItem {
+                    Image(systemName: "2.square.fill")
+                    Text("Ranking")
+                }
+            Text("History Tab")
+                .tabItem {
+                    Image(systemName: "3.square.fill")
+                    Text("History")
+                }
+        }
+        .font(.headline)
     }
 }
 
