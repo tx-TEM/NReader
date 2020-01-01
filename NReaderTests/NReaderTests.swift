ae05795b598508aa7ef685dc3293ef8573e7a601
tx-TEM <highhigheggman@yahoo.co.jp>
2020-01-01 09:17:25 +0900

    init

---
+++ NReaderTests/NReaderTests.swift
@@ -0,0 +1,34 @@
+//
+//  NReaderTests.swift
+//  NReaderTests
+//
+//  Created by yoshiki-t on 2020/01/01.
+//  Copyright © 2020 yoshiki-t. All rights reserved.
+//
+
+import XCTest
+@testable import NReader
+
+class NReaderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
